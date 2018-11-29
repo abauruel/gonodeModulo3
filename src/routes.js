45a60850438ae79d9d4762ae1ea23d9cb98fcf98
Alex.Claude <alex.claude@cmng.com.br>
2018-11-29 14:56:20 -0200

    Adicionado validação na api

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,35 @@ const express = require('express')
 const routes = express.Router()
 const authMiddleware = require('./app/middlewares/auth')
 const controllers = require('./app/controllers')
+const validate = require('express-validation')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/session', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/session',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
 /** AD */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /** purchases */
 
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
